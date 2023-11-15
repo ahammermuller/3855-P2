246a25ba817e461b3cdc9b89b62b97620674ad12
Avi <91754326+AviSob@users.noreply.github.com>
2023-11-15 15:32:19 -0800

    added another pvc

--- runall.ps1
+++ runall.ps1
@@ -1,5 +1,10 @@
 $folders = Get-ChildItem -Directory
 
+kubectl apply -f .\namespace.yml
+kubectl apply -f .\pv.yml
+kubectl apply -f .\pvc.yml
+
+
 foreach($folder in $folders){
     $files = get-ChildItem -Path $folder -Name
     foreach($file in $files){
