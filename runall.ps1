99805c9fe027ffcd44e64aeb9b483adadf13cd41
Hyewon Kim <ericak611@gmail.com>
2023-11-15 15:49:09 -0800

    3 pvc and 3 pv

--- runall.ps1
+++ runall.ps1
@@ -1,8 +1,10 @@
-$folders = Get-ChildItem -Directory
 
-foreach($folder in $folders){
-    $files = get-ChildItem -Path $folder -Name
-    foreach($file in $files){
+kubectl apply -f ./namespace.yml
+
+$folders = "Volumes", "Authentication", "File_System", "MySQL", "Upload", "Video_Streaming"  # Add your folder names in the desired order
+foreach ($folder in $folders) {
+    $files = Get-ChildItem -Path $folder -Name
+    foreach ($file in $files) {
         kubectl apply -f $folder\$file
     }
 }
