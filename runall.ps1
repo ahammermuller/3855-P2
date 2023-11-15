2d33ddb13b5dc1c79ba9c97bef873ec2c55022ab
Avi <91754326+AviSob@users.noreply.github.com>
2023-11-15 15:45:56 -0800

    CLEANED UP UPLOAD AND FILESYS

--- runall.ps1
+++ runall.ps1
@@ -1,11 +1,10 @@
 $folders = Get-ChildItem -Directory
 
 kubectl apply -f .\namespace.yml
-kubectl apply -f .\pv.yml
-kubectl apply -f .\pv2.yml
-kubectl apply -f .\pvc.yml
-kubectl apply -f .\pvc2.yml
-
+kubectl apply -f .\pv-filesys.yml
+kubectl apply -f .\pv-upload.yml
+kubectl apply -f .\pvc-filesys.yml
+kubectl apply -f .\pvc-upload.yml
 
 foreach($folder in $folders){
     $files = get-ChildItem -Path $folder -Name
