e29f5ae065e73fe059537bd1329b5872f32fbcf2
Avi <91754326+AviSob@users.noreply.github.com>
2023-11-15 13:25:49 -0800

    added the namespace run to script

--- runall.ps1
+++ runall.ps1
@@ -1,5 +1,7 @@
 $folders = Get-ChildItem -Directory
 
+kubectl apply -f .\namespace.yml
+
 foreach($folder in $folders){
     $files = get-ChildItem -Path $folder -Name
     foreach($file in $files){
