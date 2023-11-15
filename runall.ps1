9ed334e24d3fd3029092c14cc76f2c2aa54d78ba
Avi <91754326+AviSob@users.noreply.github.com>
2023-11-15 15:34:37 -0800

    modified script

--- runall.ps1
+++ runall.ps1
@@ -2,7 +2,9 @@ $folders = Get-ChildItem -Directory
 
 kubectl apply -f .\namespace.yml
 kubectl apply -f .\pv.yml
+kubectl apply -f .\pv2.yml
 kubectl apply -f .\pvc.yml
+kubectl apply -f .\pvc2.yml
 
 
 foreach($folder in $folders){
